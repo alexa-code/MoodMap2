c2e079f07696e986c075403572e8d040c8ee2313
Alexa <alexacgreenberg@gmail.com>
2014-04-08 15:13:54 -0400

    remove io config

--- app.js
+++ app.js
@@ -19,11 +19,6 @@ var twit = new twitter({
 }),
 s = null;
 
-io.configure(function() {
-	io.set("transports", ["xhr-polling"]);
-	io.set("polling duration", 10);
-});
-
 io.sockets.on('connection', function (socket) {
 	socket.on("start tweets", function() {
 		if (s == null) {
