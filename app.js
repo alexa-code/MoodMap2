b8e57cf98437ca3fa752b92a3a2b91940b7374ed
Alexa <alexacgreenberg@gmail.com>
2014-04-08 10:19:14 -0400

    fix heroku ws error?

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ var twit = new twitter({
 }),
 s = null;
 
+io.configure(function() {
+	io.set("transports", ["xhr-polling"]);
+	io.set("polling duration", 10);
+});
+
 io.sockets.on('connection', function (socket) {
 	socket.on("start tweets", function() {
 		if (s == null) {
@@ -28,12 +33,12 @@ io.sockets.on('connection', function (socket) {
 					var polarity = analyze(data.text).score;
 					// Adding to the sentimental analysis
 					if (data.coordinates && polarity !== 0 && !((data.text).match(/@|http/g))) {
+						if ((data.text).match(/:\/|stupid|ughh/g)) {
+							polarity = -4;
+						}
 						if ((data.text).match(/sad|hate|sick|tired|no good|stfu|irritat|annoy|hurt|giving up|i hate|:\(/g)) {
 							polarity = -5;
 						} 
-						if ((data.text).match(/:\/|stupid/g)) {
-							polarity = -4;
-						}
 						if ((data.text).match(/:\)|love|yay|:D|\(:/g)) {
 							polarity = 5;
 						}
